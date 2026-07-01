a3cd890ed50001e995adcd746cdf9d1415c4432f
Tanishk Sancheti <tanishksancheti1@gmail.com>
2026-07-01 19:22:38 +0530

    Index file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import logRoutes from "./routes/logs.js";
+import serverRoutes from "./routes/server.js";
+import { errorHandler } from "./middleware/errorHandler.js";
 import { startScheduler } from "./jobs/scheduler.js";
 
 dotenv.config();
@@ -14,8 +17,14 @@ app.get('/health',(req,res) =>{
     res.json({status:"ok", timestamp: new Date().toISOString()});
 });
 
+app.use('/api/servers',serverRoutes);
+app.use('/api/logs',logRoutes);
 // Error handler (Phase 4)
+app.use((req,res)=>{
+    res.status(404).json({success:false,error:"Route Not found"});
+})
 
+app.use(errorHandler);
 // Scheduler (Phase 3)
 startScheduler();
 
